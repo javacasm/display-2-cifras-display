4f176b15380074f07b3dd1d49b89dcf8b14646d9
Jose Antonio Vacas <javacasm@gmail.com>
2022-03-10 14:40:52 +0100

    Actualizar pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
+def muestraNumero2Cifras(numero: number):
+    if numero == 17:
+        basic.show_leds("""
+            # . # # #
+                        # . . . #
+                        # . . # .
+                        # . . # .
+                        # . . # .
+        """)
+    elif numero == 19:
+        basic.show_leds("""
+            # . # # #
+                        # . # . #
+                        # . # # #
+                        # . . . #
+                        # . . . #
+        """)
+    elif numero == 18:
+        basic.show_leds("""
+            # . . # .
+                        # . # . #
+                        # . . # .
+                        # . # . #
+                        # . . # .
+        """)
+    elif numero == 10:
+        basic.show_leds("""
+            # . . # .
+                        # . # . #
+                        # . # . #
+                        # . # . #
+                        # . . # .
+        """)
+    elif numero == 11:
+        basic.show_leds("""
+            # . . # .
+                        # . . # .
+                        # . . # .
+                        # . . # .
+                        # . . # .
+        """)
+    else:
+        basic.show_leds("""
+            # # # # #
+                        # # # . #
+                        # # # # #
+                        . # . . #
+                        . # . . #
+        """)
+
 def on_forever():
-    basic.show_leds("""
-                # . # # #
-                # . . . #
-                # . . # .
-                # . . # .
-                # . . # .
-    """)
-    basic.show_leds("""
-                # . . # .
-                # . # . #
-                # . . # .
-                # . # . #
-                # . . # .
-    """)
-    basic.show_leds("""
-                # . # # #
-                # . # . #
-                # . # # #
-                # . . . #
-                # . . . #
-    """)
+    muestraNumero2Cifras(18)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,56 @@
+function muestraNumero2Cifras (numero: number) {
+    if (numero == 17) {
+        basic.showLeds(`
+            # . # # #
+            # . . . #
+            # . . # .
+            # . . # .
+            # . . # .
+            `)
+    } else if (numero == 19) {
+        basic.showLeds(`
+            # . # # #
+            # . # . #
+            # . # # #
+            # . . . #
+            # . . . #
+            `)
+    } else if (numero == 18) {
+        basic.showLeds(`
+            # . . # .
+            # . # . #
+            # . . # .
+            # . # . #
+            # . . # .
+            `)
+    } else if (numero == 10) {
+        basic.showLeds(`
+            # . . # .
+            # . # . #
+            # . # . #
+            # . # . #
+            # . . # .
+            `)
+    } else if (numero == 11) {
+        basic.showLeds(`
+            # . . # .
+            # . . # .
+            # . . # .
+            # . . # .
+            # . . # .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # . #
+            # # # # #
+            . # . . #
+            . # . . #
+            `)
+    }
+}
 basic.forever(function () {
+    muestraNumero2Cifras(18)
     basic.showLeds(`
         # . # # #
         # . # . #
